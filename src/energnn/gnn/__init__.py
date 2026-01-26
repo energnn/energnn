0ce61bb9cdd1a49824f3537ac7efa098590b2715
Steve NOUATIN <nouatinste@gm0winl909.bureau.si.interne>
2026-01-26 15:18:35 +0100

    Fixed MultiFeatureTDigestNorm import
    Signed-off-by: Steve NOUATIN <nouatinste@gm0winl909.bureau.si.interne>

--- src/energnn/gnn/__init__.py
+++ src/energnn/gnn/__init__.py
@@ -9,7 +9,7 @@ from .decoder import InvariantDecoder, EquivariantDecoder
 from .encoder import Encoder, IdentityEncoder, MLPEncoder
 from .gnn import EquivariantGNN, InvariantGNN
 from .utils import MLP, gather, scatter_add
-from .normalizer import MultiFeatureTDigestNorm, GraphTDigestNorm, GraphBatchNorm
+from .normalizer import GraphTDigestNorm, GraphBatchNorm
 
 __all__ = [
     "EquivariantGNN",
@@ -20,7 +20,6 @@ __all__ = [
     "InvariantDecoder",
     "EquivariantDecoder",
     "Coupler",
-    "MultiFeatureTDigestNorm",
     "GraphTDigestNorm",
     "GraphBatchNorm",
     "MLP",
